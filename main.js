285d421be09271eca077893176f00c80b5b8a0d6
hongdlex2 <ghdals1997@naver.com>
2022-05-22 10:27:08 +0900

    jump

--- main.js
+++ main.js
@@ -33,24 +33,47 @@ class Cactus{
 
 var timer = 0;
 var cactuses = [];
+var jumpTimer =0;
 
 function animate(){
     requestAnimationFrame(animate);
     timer++;
     ctx.clearRect(0, 0 , canvas.width, canvas.height);
-
+    
     if(timer % 120 === 0){
+    //초당 표현하기 위해서 조건문 사용 
+    //현재 모니터 프레임이 120hz이면 120프레임 당 한번 표현
         var cactus = new Cactus();
         cactuses.push(cactus);
         
     }
     
-    cactuses.forEach((a)=>{
-        a.x--;
+    cactuses.forEach((a, i, o)=>{
+        if(a.x < 0){
+            o.splice(i, 1)
+        }
+        //배열의 공간낭비를 방지하기 위해서 
+        //지나간 cactuses는 제거
         a.draw();
     })
-    dino.draw();
+
+    if(jump == true){
+        dino.y--;
+        jumpTimer++;
+    }
+    if(jumpTimer > 100){
+        jump = false;
+    }
+    dino.draw()
     
 }
 
 animate();
+
+var jump = false;
+
+document.addEventListener('keydown', function(e){
+    if(e.code === 'Space'){
+        jump =true;
+    }
+})
