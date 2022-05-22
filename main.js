a80321c91e2a41d3babb9e6fdc5cbed2bab56bf7
hongdlex2 <ghdals1997@naver.com>
2022-05-22 11:22:33 +0900

    collision

--- main.js
+++ main.js
@@ -34,15 +34,16 @@ class Cactus{
 var timer = 0;
 var cactuses = [];
 var jumpTimer =0;
+var animation;
 
 function animate(){
-    requestAnimationFrame(animate);
+    animation = requestAnimationFrame(animate);
     timer++;
     ctx.clearRect(0, 0 , canvas.width, canvas.height);
     
-    if(timer % 120 === 0){
+    if(timer % 200 === 0){
     //초당 표현하기 위해서 조건문 사용 
-    //현재 모니터 프레임이 120hz이면 120프레임 당 한번 표현
+    //현재 모니터 프레임이 144z이면 144프레임 당 한번 표현
         var cactus = new Cactus();
         cactuses.push(cactus);
         
@@ -50,10 +51,13 @@ function animate(){
     
     cactuses.forEach((a, i, o)=>{
         if(a.x < 0){
-            o.splice(i, 1)
+            o.splice(i, 1);
         }
         //배열의 공간낭비를 방지하기 위해서 
         //지나간 cactuses는 제거
+        
+        a.x--;
+        collision(dino, a);
         a.draw();
     })
 
@@ -61,14 +65,35 @@ function animate(){
         dino.y--;
         jumpTimer++;
     }
+    if(jump == false){
+        if(dino.y < 200){
+            dino.y++;
+        }
+        
+    }
     if(jumpTimer > 100){
         jump = false;
+        jumpTimer = 0;
     }
-    dino.draw()
+    dino.draw();
     
 }
 
 animate();
+
+//충돌
+function collision(dino, cactus){
+    var col_x = cactus.x - (dino.x + dino.width);
+    var col_Y = cactus.y - (dino.y + dino.height);
+
+    if(col_x < 0 && col_Y < 0){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        cancelAnimationFrame(animation);
+
+    }
+}
+
+
 
 var jump = false;
 
